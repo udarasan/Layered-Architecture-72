fcc5164583df138eef8e171b461098022218e3b3
udarasan <udarassanjeewa@gmail.com>
2025-01-21 10:37:15 +0530

    ITEM BO IMPL UPDATE

--- src/main/java/com/example/layeredarchitecture/bo/CustomerBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/bo/CustomerBOImpl.java
@@ -7,42 +7,38 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerBOImpl {
+    CustomerDAOImpl customerDAO = new CustomerDAOImpl();
     @Override
     public ArrayList<CustomerDTO> getAll() throws SQLException, ClassNotFoundException {
+        return customerDAO.getAll();
     }
 
     @Override
     public void save(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
         customerDAO.save(customerDTO);
     }
 
     @Override
     public void update(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
-    CustomerDAOImpl customerDAO = new CustomerDAOImpl();
     customerDAO.update(customerDTO);
     }
 
     public void delete(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
         customerDAO.delete(id);
     }
 
     @Override
     public boolean exit(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
         return customerDAO.exit(id);
     }
 
     @Override
     public String generateNewID() throws SQLException, ClassNotFoundException {
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
         return customerDAO.generateNewID();
     }
 
     @Override
     public CustomerDTO search(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAOImpl customerDAO = new CustomerDAOImpl();
         return customerDAO.search(id);
     }
 }
--- src/main/java/com/example/layeredarchitecture/bo/ItemBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/bo/ItemBOImpl.java
@@ -1,4 +1,43 @@
 package com.example.layeredarchitecture.bo;
 
+import com.example.layeredarchitecture.dao.custom.impl.ItemDAOImpl;
+import com.example.layeredarchitecture.model.ItemDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ItemBOImpl {
+    ItemDAOImpl itemDAO = new ItemDAOImpl();
+    @Override
+    public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
+        return itemDAO.getAll();
+    }
+
+
+    @Override
+    public void save(ItemDTO item) throws SQLException, ClassNotFoundException {
+        itemDAO.save(item);
+    }
+
+    @Override
+    public void update(ItemDTO itemDTO) throws SQLException,ClassNotFoundException{
+        itemDAO.update(itemDTO);
+
+    }
+    public void delete(String code) throws SQLException, ClassNotFoundException {
+        itemDAO.delete(code);
+    }
+
+    @Override
+    public ItemDTO search(String code) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+    @Override
+    public boolean exit(String code) throws SQLException, ClassNotFoundException {
+        return itemDAO.exit(code);
+    }
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return itemDAO.generateNewID();
+    }
 }
